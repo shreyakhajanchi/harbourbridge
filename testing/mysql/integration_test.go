d5ced1537cc4d9e96fba5c95afd9a7847689e7e1
Shreya Khajanchi <khajanchi@google.com>
2021-12-01 12:30:17 +0530

    added more logs

--- testing/mysql/integration_test.go
+++ testing/mysql/integration_test.go
@@ -357,6 +357,9 @@ func checkBigInt(ctx context.Context, t *testing.T, client *spanner.Client) {
 			t.Fatal(err)
 		}
 		if err := row.Columns(&quantity); err != nil {
+			print("\ncolumn quantity")
+			print(quantity)
+			print("\n")
 			t.Fatal(err)
 		}
 	}
